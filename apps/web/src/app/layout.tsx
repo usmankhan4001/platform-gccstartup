dc9a603bb3201acb20009c8f20cf25b4da45fa48
usmankhan4001 <ranausmankhan4001@gmail.com>
2026-09-09 20:34:08 +0500

    fix(fonts): correct relative font path in layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,10 +14,10 @@ const satoshi = localFont({
   variable: '--font-satoshi',
   display: 'swap',
   src: [
-    { path: '../../../public/fonts/Satoshi-Regular.woff2', weight: '400', style: 'normal' },
-    { path: '../../../public/fonts/Satoshi-Medium.woff2', weight: '500', style: 'normal' },
-    { path: '../../../public/fonts/Satoshi-Bold.woff2', weight: '700', style: 'normal' },
-    { path: '../../../public/fonts/Satoshi-Black.woff2', weight: '900', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Regular.woff2', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Medium.woff2', weight: '500', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Bold.woff2', weight: '700', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Black.woff2', weight: '900', style: 'normal' },
   ],
 })
 
@@ -25,17 +25,17 @@ const clash = localFont({
   variable: '--font-clash',
   display: 'swap',
   src: [
-    { path: '../../../public/fonts/ClashDisplay-Regular.woff2', weight: '400', style: 'normal' },
-    { path: '../../../public/fonts/ClashDisplay-Medium.woff2', weight: '500', style: 'normal' },
-    { path: '../../../public/fonts/ClashDisplay-Semibold.woff2', weight: '600', style: 'normal' },
-    { path: '../../../public/fonts/ClashDisplay-Bold.woff2', weight: '700', style: 'normal' },
+    { path: '../../public/fonts/ClashDisplay-Regular.woff2', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/ClashDisplay-Medium.woff2', weight: '500', style: 'normal' },
+    { path: '../../public/fonts/ClashDisplay-Semibold.woff2', weight: '600', style: 'normal' },
+    { path: '../../public/fonts/ClashDisplay-Bold.woff2', weight: '700', style: 'normal' },
   ],
 })
 
 const magnita = localFont({
   variable: '--font-magnita',
   display: 'swap',
-  src: [{ path: '../../../public/fonts/Magnita.woff2', weight: '400', style: 'normal' }],
+  src: [{ path: '../../public/fonts/Magnita.woff2', weight: '400', style: 'normal' }],
 })
 
 const jbMono = JetBrains_Mono({
